4d622b5715a480f504c9909e47fed09f472617c7
GerardoQ <gerardoquispe65@gmail.com>
2020-08-29 14:14:46 -0500

    Updates

---
+++ precache-manifest.323e2cb8d8b3dc723e6de46824b740b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b03a263749841d91e52cc1c594fee4d",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "66b808689bfba225f6d5",
+    "url": "/Pokedex/static/css/main.193c589e.chunk.css"
+  },
+  {
+    "revision": "f4dc003ce1b497ee7f14",
+    "url": "/Pokedex/static/js/2.5aabc05c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.5aabc05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b808689bfba225f6d5",
+    "url": "/Pokedex/static/js/main.84f42612.chunk.js"
+  },
+  {
+    "revision": "7258a530aa9e30488b1f",
+    "url": "/Pokedex/static/js/runtime-main.5e3fa9d4.js"
+  }
+]);
